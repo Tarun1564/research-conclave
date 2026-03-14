9771734e4a2b86da9edd7a2c496067f7ec98b877
Kondaka Tarun Sai <113244858+Tarun1564@users.noreply.github.com>
2026-03-14 21:43:16 +0530

    Modify CSRF trusted origins and static URL
    Updated CSRF trusted origins and static URL path.

--- research_conclave/settings.py
+++ research_conclave/settings.py
@@ -130,14 +130,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS=['https://*.ngrok-free.app']
+CSRF_TRUSTED_ORIGINS=['https://research-conclave-production.up.railway.app']
