f2958b2a90e06be712588157f99af8538c7097ca
sailanda <landasai321@gmail.com>
2026-03-14 14:20:27 +0530

    Updated PostgreSQL database configuration

--- research_conclave/settings.py
+++ research_conclave/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'research_conclave.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'research_conclave',
+        'USER': 'research_conclave_user',
+        'PASSWORD': '9aCNDDDoCD8IKLXdMjdtRV7nMKCTVtXo',
+        'HOST': 'dpg-d6qhsg7kijhs73b65s10-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
 
