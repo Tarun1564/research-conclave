d2ea326ca088421cf1c91016917296fec76cafe2
Kondaka Tarun Sai <113244858+Tarun1564@users.noreply.github.com>
2026-03-14 21:53:15 +0530

    Fix syntax for SECURE key in CLOUDINARY_STORAGE

--- research_conclave/settings.py
+++ research_conclave/settings.py
@@ -45,7 +45,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dfbnvfee5',
     'API_KEY': '963728186599966',
     'API_SECRET': 'CMZK4FLiF93UzeyXhxXFcGWIF-w',
-    SECURE:True
+    'SECURE':True
 }
 import cloudinary
 
